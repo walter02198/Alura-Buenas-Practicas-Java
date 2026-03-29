f4cfdac86a36cc54028635252fdfee4ae2baba91
walter02198 <waltervalverdesalinas@gmail.com>
2026-03-29 15:53:33 -0400

    Se cambio idSolicitu por idAdopcion

--- src/main/java/com/aluracursos/adopet/api/dto/ReprobacionAdopcionDTO.java
+++ src/main/java/com/aluracursos/adopet/api/dto/ReprobacionAdopcionDTO.java
@@ -1,5 +1,5 @@
 package com.aluracursos.adopet.api.dto;
 
-public record ReprobacionAdopcionDTO(Long idSolicitud,
+public record ReprobacionAdopcionDTO(Long idAdopcion,
                                      String justificacion) {
 }
